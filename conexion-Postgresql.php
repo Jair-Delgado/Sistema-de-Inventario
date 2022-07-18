ebdc8987175723419b2df73d91c3860a1445ab44
Jair-Delgado <jar.delgado@yavirac.edu.ec>
2022-07-18 11:35:46 -0500

    navegacion pag principal

--- conexion-Postgresql.php
+++ conexion-Postgresql.php
@@ -1,5 +1,3 @@
 <?php
-
 $conexion = pg_connect ("host=localhost dbname=inventario user=postgres password=1234");
-
 ?>
--- ingreso.php
+++ ingreso.php
@@ -7,6 +7,7 @@
         <link rel="icon" href="img/inventario.png">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Oswald:wght@200;400&family=Roboto:wght@100&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="estilos/estilo-ingreso.css">
         <title>Sistema de Inventario</title>
  </head>
  <body>
@@ -14,25 +15,83 @@
 
       
        <!--Mensaje de Bienvenida al usuario--> 
-       <div class="welcome-message">
+       
+       <nav class="navegacion">
               <div class="icono-usuario"> <i class="fa-solid fa-user"></i> </div>
               <?php
                      session_start();
                      $usuario=$_SESSION['nombre_usuario'];
                      echo "<h1>Bienvenido $usuario</h1>";     
               ?>
+              
 
-       </div>
-
-       <nav class="navegacion">
-
+              <h1>MENU INVENTARIO</h1> <a id="cerrar-sesion" href="index.php">Cerrar Sesion</a>
        </nav>
 
 
 
-        <h1>MENU INVENTARIO</h1> 
-        <a href="index.php">Cerrar Sesion</a>
+        
        </div>
+
+       <div class="con">
+            <div class="contenedor">
+                <header>
+                    <h2>GESTOR DE PRODUCTOS</h2>
+                </header>
+    
+                <main>
+                    <a href="#">
+                        <section>
+                            <h2>Consultar<br>stock<br>productos</h2>
+                        </section>
+                    </a>
+    
+                    <a href="#">
+                        <section>
+                            <h2>Realizar<br>pedido<br>a proveedor</h2>
+                        </section>
+                    </a>
+    
+                    <a href="#">
+                        <section>
+                            <h2>Registrar<br>entrega<br>del proveedor</h2>
+                        </section>
+                    </a>
+    
+                    
+    
+                </main>
+            </div>
+
+            <aside>
+                <h3>Productos stock bajo</h3><hr><br><br>
+                <table>
+                    <tr>
+                        <th>producto</th>
+                        <th>stock</th>
+                    </tr>
+                    <tr>
+                        <td>nombre</td>
+                        <td>stock</td>
+                    </tr>
+                    <tr>
+                        <td>nombre</td>
+                        <td>stock</td>
+                    </tr>
+                    <tr>
+                        <td>nombre</td>
+                        <td>stock</td>
+                    </tr>
+                    <tr>
+                        <td>nombre</td>
+                        <td>stock</td>
+                    </tr>
+
+                </table>
+              </aside>
+              
+       </div>
+       
  </body>
  </html>
  
