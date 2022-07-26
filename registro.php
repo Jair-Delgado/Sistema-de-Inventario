1b0b32a0bd5babad3f35ad3bafd3ed312c2afc6b
Jair-Delgado <jar.delgado@yavirac.edu.ec>
2022-07-26 08:47:39 -0500

    Update registro.php

--- registro.php
+++ registro.php
@@ -10,8 +10,8 @@ class Usuarios
 
         #Datos del formulario de registro
         $cedula = $_POST["cedula"];
-        $nombre = $_POST["nombre"];
-        $apellido = $_POST["apellido"];
+        $nombre = strtolower($_POST["nombre"]);
+        $apellido = strtolower($_POST["apellido"]);
         $email = $_POST["email"];
         $telefono = $_POST["telefono"];
         $contraseña = $_POST["contraseña"];
