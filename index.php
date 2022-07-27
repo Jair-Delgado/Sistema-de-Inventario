7df8ecbbc1f1f3a8c60b44e200c81add2bdd7d1b
ChatonexX <leolmini_99@hotmail.com>
2022-07-27 17:51:36 -0500

    actualizacion-index

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilos/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Edu+VIC+WA+NT+Beginner&family=Lobster&family=Merriweather+Sans:ital,wght@0,600;1,300&family=Oswald:wght@500&family=Pacifico&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Merriweather+Sans:ital,wght@0,600;1,300&family=Pacifico&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Oswald:wght@200;400&family=Roboto:wght@100&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400&family=Roboto:wght@300&display=swap" rel="stylesheet">
     <title>Bienvenido</title>
@@ -14,17 +18,26 @@
 <body>
     <header>
        
-        <div class="app-name">
+        <div class="sisi">
             <h1>SISTEMA DE INVENTARIO</h1>
-
         </div>
     </header>
-    
-    <div class="text">
 
-        <h2>Porfavor Inicie Sesion o Registrese </h2> 
-        <a href="login.php"> <b>Iniciar Sesión</b></a> o
-        <a href="registro-usuarios.php"> <b>Registrarse</b></a>
+    <div>
+
+        <h2>Porfavor Inicie Sesion o Registrese </h2>
+
+        <div class="text">
+
+        <a href="login.php" class="index"> Iniciar Sesión </a>
+
+        <a href="registro-usuarios.php" class="index"> Registrarse </a>
+        
+        </div>
+
     </div>
+
+    
+
 </body>
 </html>
